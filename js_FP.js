e00e8d670ac78caa13ed417d4a1af8b8429798a5
Zeno <ng377925@gmail.com>
2025-03-11 17:04:38 +0530

    add FP code0

---
+++ js_FP.js
@@ -0,0 +1,13 @@
+// currency exchange
+
+var currency1 = 100;
+var currency2 = 0;
+var exchangeRate = 1.2;
+
+function convertCurrency(amount, rate) {
+    return amount * rate;
+}
+
+currency2 = convertCurrency(currency1, exchangeRate);
+
+console.log(currency2);
